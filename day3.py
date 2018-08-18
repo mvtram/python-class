9863f86295ad743bc49b8d98d1ec44be0af63098
mvtram <34829666+mvtram@users.noreply.github.com>
2018-08-18 16:11:31 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 18 14:37:02 2018
+
+@author: mahesh
+"""
+
+import pymysql as sql
+#milk distribution system
+"""
+first customer is admin himself and first he will add the product price and quantity
+the admin will add the customer by generating customer key
+customer info-name address
+customer type-shop,individual
+3 relation between customer and admin
+many to manyb relation
+
+
+"""
